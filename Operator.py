2862efeee6234353cb5c80ff9de3134955943e88
Imteaz Alam Prince <295625598+iaprince28@users.noreply.github.com>
2026-08-13 17:49:16 +0600

    Added folder

---
+++ Operator.py
@@ -0,0 +1,58 @@
+# #Python arithmetic operator
+
+# a = 10
+# b = 20
+
+# #Addition
+# print(a + b)
+
+# #Subtraction
+# print(b - a)
+
+# #Multiplication
+# print(a * b)
+
+# #Division
+# print(a / b)
+
+# #Modulus
+# print(b % a)
+
+# #Exponentiation
+# print(a ** b)
+
+# #Floor division
+# sum1 = 15
+# sum2 = 2
+
+# print(sum1 // sum2)
+
+
+# Assignment operator
+
+# a = 5
+# sum = a + 5
+
+# print(sum)
+
+# sum += 10
+
+# print(sum)
+
+a = 10
+
+a -= 50
+
+print(a)
+
+#Swaping
+
+c = 50
+d = 60
+
+c,d = d,c
+ 
+# print(c,d)
+
+print("The value of c is", c)
+print("The value of d is", d )
