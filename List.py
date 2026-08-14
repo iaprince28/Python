e7412c265b56ba19afed52d37e51b94078c3b997
Imteaz Alam Prince <295625598+iaprince28@users.noreply.github.com>
2026-08-14 23:48:49 +0600

    file added

--- List.py
+++ List.py
@@ -9,4 +9,29 @@ print(li)
 
 li = ["prince", "imteaz", "alam"]
 
+print(type(li))
 print(li)
+
+li = [True, False, True, False]
+
+print(type(li))
+print(li)
+
+#Access list iteem
+
+Access = ['WSPN', 'RAW', 'WWE', 'ME']
+
+
+print(Access[3])
+
+# Chnage list iteem
+
+Access[3] = 'EEE'
+
+print(Access)
+
+# Append
+
+Access.append(10)
+
+print(Access)
