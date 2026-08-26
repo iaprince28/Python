9fd6923cc53d755e5e3a17b34ecac99344090c77
Imteaz Alam Prince <295625598+iaprince28@users.noreply.github.com>
2026-08-27 00:33:06 +0600

    file changed

--- List.py
+++ List.py
@@ -71,7 +71,7 @@ print(li)
 
 #For loop
 
-loopList = ['Ankita', 'Totini', 'Niha', 'Tabassum', 'Orthi', 'Ognila']
+loopList = ['Ankita', 'Totini', 'Niha', 'Tabassum', 'Orthi', 'Ognila','Ohona']
 
 for fahim in loopList:
     print(fahim)
